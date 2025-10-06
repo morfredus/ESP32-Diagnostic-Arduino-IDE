4523b5c91214cdbecf9883ab0ed553110a8b23b2
morfredus <morfredus@gmail.com>
2025-10-06 17:12:49 +0200

    Ajout d'un fichier config.h exemple à renommer
    Exemple de fichier config.h, à renommer config.h et à compléter avant la première exécution.
    
    Here is an example of a config.h file to rename and complete before the first execution.

---
+++ exemple-config.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// ========== CONFIGURATION WIFI ==========
+// renommez ce fichier en config.h et ajoutez vos reseaux WIFI
+// Ajoutez vos réseaux WiFi ici
+const char* WIFI_SSID_1 = "VotreSSID1";
+const char* WIFI_PASS_1 = "VotreMotDePasse1";
+
+const char* WIFI_SSID_2 = "VotreSSID2";
+const char* WIFI_PASS_2 = "VotreMotDePasse2";
+
+// Ajoutez autant de réseaux que nécessaire
+// const char* WIFI_SSID_3 = "VotreSSID3";
+// const char* WIFI_PASS_3 = "VotreMotDePasse3";
+
+#endif
