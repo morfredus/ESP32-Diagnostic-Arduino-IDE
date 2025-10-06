4e8a5c487cbcd77d47334ec41e3bda680e0cda9d
morfredus <morfredus@gmail.com>
2025-10-06 17:24:51 +0200

    uniformisation des nom SSID et Mot de passe

--- exemple-config.h
+++ exemple-config.h
@@ -2,16 +2,16 @@
 #define CONFIG_H
 
 // ========== CONFIGURATION WIFI ==========
-// renommez ce fichier en config.h et ajoutez vos reseaux WIFI
+// renommez ce fichier en config.h et ajoutez votre (vos) reseaux WIFI
 // Ajoutez vos réseaux WiFi ici
-const char* WIFI_SSID_1 = "VotreSSID1";
-const char* WIFI_PASS_1 = "VotreMotDePasse1";
-
-const char* WIFI_SSID_2 = "VotreSSID2";
-const char* WIFI_PASS_2 = "VotreMotDePasse2";
+const char* WIFI_SSID_1 = "SSID1";
+const char* WIFI_PASS_1 = "MotDePasse1";
 
 // Ajoutez autant de réseaux que nécessaire
-// const char* WIFI_SSID_3 = "VotreSSID3";
-// const char* WIFI_PASS_3 = "VotreMotDePasse3";
+// const char* WIFI_SSID_2 = "SSID2";
+// const char* WIFI_PASS_2 = "MotDePasse2";
+
+// const char* WIFI_SSID_3 = "SSID3";
+// const char* WIFI_PASS_3 = "MotDePasse3";
 
 #endif
