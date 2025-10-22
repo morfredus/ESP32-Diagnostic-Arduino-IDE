09e3a2de03f879b27ced9b9f48c2d4c78e526220
morfredus <morfredus@gmail.com>
2025-10-23 00:44:07 +0200

    Create wifi-config-example.h for WiFi settings
    Add example WiFi configuration file with placeholders.

---
+++ wifi-config-example.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// ========== CONFIGURATION WIFI ==========
+// renommez ce fichier en config.h et ajoutez votre (vos) reseaux WIFI
+// Ajoutez vos réseaux WiFi ici
+const char* WIFI_SSID_1 = "SSID1";
+const char* WIFI_PASS_1 = "MotDePasse1";
+
+// Ajoutez autant de réseaux que nécessaire
+// const char* WIFI_SSID_2 = "SSID2";
+// const char* WIFI_PASS_2 = "MotDePasse2";
+
+#endif
