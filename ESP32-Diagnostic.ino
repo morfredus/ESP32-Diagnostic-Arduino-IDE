52463ff36d42f035a3b4fb028c823041cee30a6d
morfredus <morfredus@gmail.com>
2025-11-05 18:05:16 +0100

    Précision sur la carte utilisée pour les tests

--- ESP32-Diagnostic.ino
+++ ESP32-Diagnostic.ino
@@ -2,7 +2,7 @@
  * ESP32 Diagnostic Suite v3.7.04-dev
  * Compatible: ESP32, ESP32-S2, ESP32-S3, ESP32-C3, ESP32-C6, ESP32-H2
  * Optimisé pour ESP32 Arduino Core 3.3.2
- * Carte testée: ESP32-S3 avec PSRAM OPI
+ * Carte testée: ESP32-S3 DevkitC-1 R16 avec PSRAM OPI
  * Auteur: morfredus
  */
 
